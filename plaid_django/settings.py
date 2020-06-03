26ed78124350811a80e04cddbbff3b14d1aaa88e
Prashant Rawat <prashant.rawat216@gmail.com>
2020-06-03 23:28:35 +0530

    DataBase Changed to CloudSQL

--- plaid_django/settings.py
+++ plaid_django/settings.py
@@ -77,12 +77,42 @@ WSGI_APPLICATION = 'plaid_django.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
+if os.getenv('GAE_APPLICATION', None):
+    # Running on production App Engine, so connect to Google Cloud SQL using
+    # the unix socket at /cloudsql/<your-cloudsql-connection string>
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.mysql',
+            'HOST': '/cloudsql/sympy-live-pr:asia-south1:sympy',
+            'NAME': 'plaid_db',
+            'USER': 'root',
+            'PASSWORD': 'n4ez4y2Fou2tMcqe',
+        }
     }
-}
+else:
+    # Running locally so connect to either a local MySQL instance or connect to
+    # Cloud SQL via the proxy. To start the proxy via command line:
+    #
+    #     $ cloud_sql_proxy -instances=[INSTANCE_CONNECTION_NAME]=tcp:3306
+    #
+    # See https://cloud.google.com/sql/docs/mysql-connect-proxy
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.mysql',
+            'HOST': '127.0.0.1',    # DataBase's IP address
+            'PORT': '3306',
+            'NAME': 'plaid_db',
+            'USER': 'root',
+            'PASSWORD': 'n4ez4y2Fou2tMcqe',
+        }
+    }
 
 
 # Password validation
