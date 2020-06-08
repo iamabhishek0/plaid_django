ab7216a8b317e81a5109c831936c3b4b09af1e18
Abhishek Chaurasia <abhi24061997@gmail.com>
2020-06-09 01:50:42 +0530

    Added Login Page which opens when user tries to open a page without logging in

--- plaid_django/urls.py
+++ plaid_django/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import include
+from plaid_link import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('plaid_link.urls')),
+    path("login/", views.loginview, name='login'),
 ]
